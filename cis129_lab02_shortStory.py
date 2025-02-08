f8b987a708bbecef3f1f40e071a14969067308c7
ianLochridgePima <ilochridge@mail.pima.edu>
2025-02-07 22:29:51 -0700

    Update cis129_lab02_shortStory.py
    Fixed an error with an apostrophe messing up the print statement in the last print line.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -5,4 +5,4 @@
 print('I first started learning program when I was around 9 years old.')
 print('One of my friends pushed me a bit too hard while we were messing around one day in a park.')
 print('His parents felt bad about the ordeal and got me a Canakit (Raspberry Pi Kit).')
-print('I started to learn python on it, and it's how I became interested in coding.')
+print('I started to learn python on it, and it is how I became interested in coding.')
